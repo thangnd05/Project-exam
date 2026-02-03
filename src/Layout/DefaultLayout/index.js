7fe666dc58c6697734e842c6daa42975723b642c
thangnd05 <thangnd.contact@gmail.com>
2026-02-03 21:28:49 +0700

    fix: bug fr defaultLayOut

--- src/Layout/DefaultLayout/index.js
+++ src/Layout/DefaultLayout/index.js
@@ -1,21 +1,31 @@
-import classNames from 'classnames/bind';
-import style from './DefaultLayout.module.scss';
-import Header from '../Header';
-import Footer from '../Footer';
-import ScrollToTop from '../SconllToTop';
+import classNames from "classnames/bind";
+import styles from "./DefaultLayout.module.scss";
+import Header from "../Header";
+import Footer from "../Footer";
+import ScrollToTop from "../SconllToTop";
 
-const cx = classNames.bind(style);
+const cx = classNames.bind(styles);
 
-function DefaultLayout({children, noContainer = false}) {
+function DefaultLayout({ children, noContainer = false }) {
   return (
-    <div className={cx('Wrapper')}>
+    <div className={cx("wrapper")}>
       <Header />
-      <div className={cx('content', {'container-lg': !noContainer})}>
-        {children}
-      </div>
+
+      {/* Main full width */}
+      <main className={cx("main")}>
+        {noContainer ? (
+          // ✅ Full width page (Home landing...)
+          children
+        ) : (
+          // ✅ Normal pages inside container
+          <div className="container-lg">{children}</div>
+        )}
+      </main>
+
       <Footer />
       <ScrollToTop />
     </div>
   );
 }
+
 export default DefaultLayout;
