ecec6ff1222dafc42f28871b2764f0e9f6825032
thangnd05 <thangnd.contact@gmail.com>
2026-07-09 21:09:08 +0700

    chore(cleanup): remove unused App.css, App.test.js, and reportWebVitals.js files to streamline the project structure

--- frontend/src/App.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-test("testing library renders a node", () => {
-  render(<div>hello</div>);
-  expect(screen.getByText("hello")).toBeInTheDocument();
-});
