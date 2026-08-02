16f754118ba3097343c248c4d3e4d731db1ebced
thangnd05 <thangnd.contact@gmail.com>
2026-08-02 14:59:49 +0700

    refactor: add tasks field to LearningMapper builder for improved task management and data handling

--- backend/src/main/java/com/project_exam/backend/modules/assessment/learning/mapper/LearningMapper.java
+++ backend/src/main/java/com/project_exam/backend/modules/assessment/learning/mapper/LearningMapper.java
@@ -148,6 +148,7 @@ public class LearningMapper {
                 .passedTasksInPart(passedTasksInPart)
                 .totalTasksInPart(totalTasksInPart)
                 .partResources(partResources)
+                .tasks(tasks)
                 .build();
     }
 
